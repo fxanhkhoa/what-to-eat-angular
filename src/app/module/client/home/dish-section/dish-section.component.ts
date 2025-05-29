4ea8daede5a94bd70b07901206deb4ebc6940579
fxanhkhoa <fxanhkhoa@gmail.com>
2025-05-29 16:29:51 +0700

    updating dish section

--- src/app/module/client/home/dish-section/dish-section.component.ts
+++ src/app/module/client/home/dish-section/dish-section.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { MEAL_CATEGORIES } from '@/enum/dish.enum';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-dish-section',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatListModule, MatIconModule],
   templateUrl: './dish-section.component.html',
-  styleUrl: './dish-section.component.scss'
+  styleUrl: './dish-section.component.scss',
 })
 export class DishSectionComponent {
+  private fb = inject(FormBuilder);
 
+  mealCategories = Object.values(MEAL_CATEGORIES);
+
+  mealCategoryForm: FormGroup = this.fb.group({
+    mealCategory: [''],
+  });
 }
